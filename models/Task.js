a743c96f96e5898daf7864e9fbfeb8a796f934e6
kaushiki t <kaushikit058@gmail.com>
2026-03-17 11:04:02 +0530

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+const taskSchema = new mongoose.Schema(
+  {
+    userId: String,
+
+    title: {
+      type: String,
+      required: true
+    },
+
+    description: String,
+
+    status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending"
+    },
+
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium"
+    },
+
+    dueDate: Date
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Task", taskSchema)
